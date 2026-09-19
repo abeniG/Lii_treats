c869b74093121ef4484efa3118a7c0b301bf4cc5
abeniG <abenikeradio@gmail.com>
2026-09-19 08:33:43 +0100

    tral

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Required for Cloudflare Pages compatibility
+  output: "standalone",
+
+  images: {
+    // Allow images served from the /public folder (local)
+    unoptimized: true, // Cloudflare Pages doesn't support Next.js Image Optimization server
+  },
 };
 
 export default nextConfig;
